be18dd8eda8d4afe364dc098167104b3745a8233
022 <022@192.168.0.62>
2018-03-13 12:11:39 +0900

    sd

--- MainProject/src/com/inc/project/ParkFrame.java
+++ MainProject/src/com/inc/project/ParkFrame.java
@@ -9,8 +9,18 @@ public class ParkFrame extends JFrame {
 		setBounds(300, 300, 500, 500);
 		setResizable(false);
 		// setLayout(new FlowLayout());
-
+		
+		init();
+		
 		setVisible(true);
+	}
+
+	private void init() {
+		initComponent();
+	}
+
+	private void initComponent() {
+		
 	}
 
 	public static void main(String[] args) {
