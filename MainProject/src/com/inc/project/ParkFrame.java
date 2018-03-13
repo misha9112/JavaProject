c2a0b161f6348a750ca6766d6fd5dc6ac6e2ee75
023 <023@192.168.0.69>
2018-03-13 12:09:35 +0900

    Park

--- MainProject/src/com/inc/project/ParkFrame.java
+++ MainProject/src/com/inc/project/ParkFrame.java
@@ -3,17 +3,32 @@ package com.inc.project;
 import javax.swing.JFrame;
 
 public class ParkFrame extends JFrame {
+	
+	JPanel parkPanel;
+	JTextArea parkArea;
+
 	public ParkFrame() {
 		setTitle("Main Frame");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setBounds(300, 300, 500, 500);
+		setBounds(500, 200, 500, 500);
 		setResizable(false);
-		// setLayout(new FlowLayout());
+		setLayout(new FlowLayout());
+
+		init();
 
 		setVisible(true);
+	}
+
+	public void init() {
+		initComponent();
+	}
+
+	public void initComponent() {
+
 	}
 
 	public static void main(String[] args) {
 		new ParkFrame();
 	}
+
 }
